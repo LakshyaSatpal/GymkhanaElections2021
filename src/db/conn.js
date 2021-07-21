503dcac37e85b74f35aacb724c48bba33cb0d885
Lakshya <lakshyasatpal372@gmail.com>
2021-07-21 16:18:32 +0530

    Added useFindAndModify to remove deprecation warning

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,6 +4,7 @@ mongoose
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("Connected Successfully");
